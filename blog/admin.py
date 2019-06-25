0e8b99ce797c33e50bdc9483b60b6f76bb49cc43
Viktror <supermario.21@mail.ru>
2019-06-25 21:31:50 +0300

    create models, create database

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models  import Post
+# Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+
+admin.site.register(Post)
